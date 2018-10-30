69ac59bdc307eeba4062977b5d79f9f4fe703105
Konstantin <44602283+littlefreelancer@users.noreply.github.com>
2018-10-31 00:00:36 +0300

    upd

--- yandex-donate.php
+++ yandex-donate.php
@@ -11,7 +11,7 @@ $true_page = 'yandex-donate.php';
  
 add_action('admin_menu', 'true_options');
 add_action( 'admin_init', 'true_option_settings' );
-add_shortcode( 'yd-button-donate', 'yd_buttom_shortcode' );
+add_shortcode( 'yd-button-donate', 'yd_button_shortcode' );
 add_action( 'admin_print_footer_scripts', 'appthemes_add_quicktags' );
 
 function true_options() {
@@ -44,7 +44,6 @@ function true_option_settings() {
 		'label_for' => 'yd_account'
 	);
 	add_settings_field( 'my_text_field_acc', 'Номер кошелька', 'true_option_display_settings', $true_page, 'global_section', $true_field_params );
- 
  	$true_field_params = array(
 		'type'      => 'text',
 		'id'        => 'yd_summa',
@@ -55,11 +54,8 @@ function true_option_settings() {
 }
 function true_option_display_settings($args) {
 	extract( $args );
- 
 	$option_name = 'true_options';
- 
 	$o = get_option( $option_name );
- 
 	switch ( $type ) {  
 		case 'text':  
 			$o[$id] = esc_attr( stripslashes($o[$id]) );
@@ -68,7 +64,7 @@ function true_option_display_settings($args) {
 		break;
 	}
 }
-function yd_buttom_shortcode()
+function yd_button_shortcode()
 {
 	$all_options = get_option('true_options');
 	$yandex_buttom_src = '<iframe src="https://money.yandex.ru/quickpay/button-widget?targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D0%B6%D0%BA%D0%B0%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0&default-sum=' . $all_options['yd_summa'] . '&button-text=13&yamoney-payment-type=on&button-size=m&button-color=orange&successURL=&quickpay=small&account=' . $all_options['yd_account'] . '" width="184" height="36" frameborder="0" allowtransparency="true" scrolling="no"></iframe>';
